319485a15520b1e206238a8c6124f4cec207a192
yonghyeon kim <yhsofa89@gmail.com>
2024-09-09 05:16:33 +0900

    수정

--- crawl_recent_oil_data.py
+++ crawl_recent_oil_data.py
@@ -107,7 +107,7 @@ def crawl_for_sido(sido_name, project_dir, sidosigun_code):
         try:
             start_time = time.time()
             # 특정 요소가 나타날 때까지 최대 10초 대기
-            WebDriverWait(driver,60).until(
+            WebDriverWait(driver, 60).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="SIDO_NM0"]'))
             )
             end_time = time.time()
@@ -138,7 +138,7 @@ def crawl_for_sido(sido_name, project_dir, sidosigun_code):
         print(f"{sido_name} 시도란 입력완료 걸린 시간 : {elapsed_time:.1f}초")
         time.sleep(2)
         # 시군란 입력       
-        sigun = WebDriverWait(driver, 10).until(
+        sigun = WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="SIGUNGU_NM0"]'))
         )
         Select(sigun).select_by_visible_text(sigun_name) # 시군 네임 입력
@@ -158,7 +158,7 @@ def crawl_for_sido(sido_name, project_dir, sidosigun_code):
         elapsed_time = end_time - start_time
         print(f"{sido_name} 시군란 입력완료 걸린 시간 : {elapsed_time:.1f}초")
         time.sleep(2)
-        excel_download_button = WebDriverWait(driver, 50).until(
+        excel_download_button = WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="templ_list0"]/div[7]/div/a'))
         )
         driver.execute_script("arguments[0].click();", excel_download_button)
@@ -214,7 +214,7 @@ def get_opinet_oildata_crawler():
     sido_list = [sido['AREA_NM'] for sido in sidosigun_code['SIDO']]
     print(f'sido list = {sido_list}')
     recent_oil_data_list = []
-    with ThreadPoolExecutor(max_workers=4) as executor:  # 스레드 풀 생성
+    with ThreadPoolExecutor(max_workers=8) as executor:  # 스레드 풀 생성
         future_to_sido = {executor.submit(crawl_for_sido, sido_name, project_dir, sidosigun_code): sido_name for sido_name in sido_list}
         for future in as_completed(future_to_sido):
             sido_name = future_to_sido[future]
