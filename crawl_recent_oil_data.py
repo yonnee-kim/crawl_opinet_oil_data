c7a26484119247c885ee992849aa0682087116b6
yonghyeon kim <yhsofa89@gmail.com>
2024-09-12 07:01:25 +0900

    ㅎㅎ

--- crawl_recent_oil_data.py
+++ crawl_recent_oil_data.py
@@ -115,6 +115,7 @@ def crawl_for_sido(sido_name, project_dir, sidosigun_code, code_start_time):
             end_time = time.time()
             elapsed_time = end_time - start_time
             print(f"{sido_name} 초기 웹페이지 로드 완료! 걸린 시간 : {elapsed_time:.1f}초")
+            time.sleep(1)
             break
         except Exception as e:
             end_time = time.time()
@@ -139,6 +140,7 @@ def crawl_for_sido(sido_name, project_dir, sidosigun_code, code_start_time):
                 end_time = time.time()
                 elapsed_time = end_time - start_time
                 print(f"{sido_name} 웹페이지 로드 성공 {elapsed_time:.1f}초:")
+                time.sleep(3)
             except Exception as e:
                 end_time = time.time()
                 elapsed_time = end_time - start_time
@@ -155,6 +157,7 @@ def crawl_for_sido(sido_name, project_dir, sidosigun_code, code_start_time):
             )
             Select(sido).select_by_visible_text(sido_name)
             start_time = time.time()
+            time.sleep(1)
             trycount = 0
             while True : 
                 try:
@@ -176,7 +179,6 @@ def crawl_for_sido(sido_name, project_dir, sidosigun_code, code_start_time):
             end_time = time.time()
             elapsed_time = end_time - start_time
             print(f"{sido_name} 시도란 입력완료 걸린 시간 : {elapsed_time:.1f}초")
-            time.sleep(1)
             # 시군란 입력       
             sigun = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="SIGUNGU_NM0"]'))
